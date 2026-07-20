9068618138e0fdfe4027ffa2637cf3be24291f31
vedanshi saini <vedanshi0619@gmail.com>
2026-07-20 21:39:37 +0530

    m

---
+++ developer/jss.js
@@ -0,0 +1,15 @@
+function lineofstar(){
+    let line="";
+
+for (let i = 0; i < 5; i++) {
+  line += "*";
+}
+
+console.log(line);
+
+}
+lineofstar();
+console.log("hello");
+lineofstar();
+console.log("world");
+lineofstar();
